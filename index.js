f5e6cfc9ca286e976a25ccfbff91d2cbf0b01e6b
Hans Naert <hans.naert@vives.be>
2023-10-13 21:50:06 +0200

    Add socket.io and connect

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import path from 'path';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-    res.sendFile('C:\\projecten\\embed\\SocketChatExample\\index.html');
-  });
+  res.sendFile(path.resolve('./index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
