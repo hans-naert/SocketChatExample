b44c28fc6710aa09cac62f1db1787e92747ed6f4
Hans Naert <hans.naert@vives.be>
2025-10-21 14:37:34 +0100

    Add socket.io - connect user and receive chat message

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 /*app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
@@ -9,6 +11,16 @@ const server = http.createServer(app);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
 });
 
 server.listen(3000, () => {
