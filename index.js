37c1e8b9f08d460cff60dcee8a7640f73f585a8b
Hans Naert <hans.naert@vives.be>
2025-10-21 15:12:52 +0100

    Add onoff, toggle io when message arrives

--- index.js
+++ index.js
@@ -5,9 +5,23 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const {Gpio} = require('onoff');
+
 /*app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });*/
+
+let input18 = new Gpio(512+18, 'in', 'both');
+let output17 = new Gpio(512+17, 'out');
+
+input18.watch((err, value) => { 
+  if (err) {
+    console.error('There was an error', err);
+    return;
+  }
+  console.log('Input 18 value changed to', value);
+  io.emit('chat message', 'Input 18 value changed to ' + value);
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -21,6 +35,7 @@ io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
      io.emit('chat message', msg);
     console.log('message: ' + msg);
+    output17.readSync() === 0 ? output17.writeSync(1) : output17.writeSync(0);
   });
 });
 
